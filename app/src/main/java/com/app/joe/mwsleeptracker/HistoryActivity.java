ec975dd02e2dfefaa773b2638f8104d0b8abf111
jfranco1701 <jfranco@unomaha.edu>
2016-04-29 22:57:29 -0500

    Graph

--- app/src/main/java/com/app/joe/mwsleeptracker/HistoryActivity.java
+++ app/src/main/java/com/app/joe/mwsleeptracker/HistoryActivity.java
@@ -1,5 +1,9 @@
 package com.app.joe.mwsleeptracker;
 
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Shader;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -9,13 +13,19 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
+import com.androidplot.Plot;
 import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
+import java.text.FieldPosition;
+import java.text.Format;
+import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
+import android.graphics.DashPathEffect;
 import com.androidplot.util.PixelUtils;
 import com.androidplot.xy.SimpleXYSeries;
 import com.androidplot.xy.XYSeries;
@@ -28,6 +38,8 @@ public class HistoryActivity extends AppCompatActivity {
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
     private GoogleApiClient client;
+
+    private XYPlot plot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,10 +57,118 @@ public class HistoryActivity extends AppCompatActivity {
         String formattedDate = df.format(date);
         tvDate.setText(formattedDate);
 
+        generateGraph();
+
+
+
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
+    private void generateGraph(){
+        plot = (XYPlot) findViewById(R.id.plot);
+
+        Number[] sleep = {0, 5, 10, 5, 10, 0, 5, 10, 10, 10, 0};
+        Number[] times = {22.5, 22.7, 23, 23.7, 24.4, 24.7, 25.5, 26.4, 27.7, 28.7, 29.2};
+
+        XYSeries series2 = new SimpleXYSeries(
+                Arrays.asList(times),
+                Arrays.asList(sleep),
+                "Sleep Quality");
+
+        LineAndPointFormatter series1Format = new LineAndPointFormatter();
+        series1Format.setPointLabelFormatter(new PointLabelFormatter());
+        series1Format.configure(getApplicationContext(), R.xml.line_point_formatter_with_labels);
+
+//        series1Format.setInterpolationParams(
+//                new CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal));
+
+        series1Format.setPointLabelFormatter(null);
+
+        plot.addSeries(series2, series1Format);
+
+        plot.setRangeStep(XYStepMode.INCREMENT_BY_VAL, 1);
+        plot.setDomainStep(XYStepMode.INCREMENT_BY_VAL,.5);
+        plot.setTicksPerRangeLabel(5);
+        plot.setTicksPerDomainLabel(4);
+        plot.setDomainLabel("");
+        plot.setRangeLabel("");
+
+        plot.setDomainValueFormat(new Format() {
+            @Override
+            public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
+                Number num = (Number) obj;
+
+                String time = "";
+                int hour = (int)Math.floor((double)num);
+                if (hour >= 24){
+                    hour = hour - 24;
+                }
+
+                double min = (double)num - hour;
+
+                time = Integer.toString(hour);
+                if (hour < 10){
+                    time = "0" + time;
+                }
+
+                if (min == .0){
+                    time = time + ":00         ";
+                }
+                else{
+                    time = time + ":30         ";
+                }
+
+                toAppendTo.append(time);
+
+                return toAppendTo;
+            }
+
+            @Override
+            public Object parseObject(String source, ParsePosition pos) {
+                return null;
+            }
+        });
+
+
+        // create a custom getFormatter to draw our state names as range tick labels:
+        plot.setRangeValueFormat(new Format() {
+            @Override
+            public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
+                Number num = (Number) obj;
+                switch(num.intValue()) {
+                    case 10:
+                        toAppendTo.append("Aslp   ");
+                        break;
+                    case 5:
+                        toAppendTo.append("Rstls   ");
+                        break;
+                    case 0:
+                        toAppendTo.append("Awk   ");
+                        break;
+                    default:
+                        toAppendTo.append("Unk   ");
+                        break;
+                }
+                return toAppendTo;
+            }
+
+            @Override
+            public Object parseObject(String source, ParsePosition pos) {
+                return null;
+            }
+        });
+
+
+
+        // rotate domain labels 45 degrees to make them more compact horizontally:
+        plot.getGraphWidget().setDomainLabelOrientation(-45);
+
+    }
+
+
+
 
     @Override
     public void onStart() {
